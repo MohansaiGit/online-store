4fd97a4c21f3f821f5bb6eb85c2d2ec6962fe945
MohansaiGit <mohansai9169@gmail.com>
2022-03-30 14:58:15 +0530

    created capcustomization

---
+++ customization/src/components/CapCustomization.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class CapCustomization extends Component {
+  render() {
+    return (
+      <div>CapCustomization</div>
+    )
+  }
+}
+
+export default CapCustomization
