8db9ff5ecba8dc347de2dbcbffeba953a0c9c7d7
RajanyaGit <102577983+RajanyaGit@users.noreply.github.com>
2022-03-31 12:21:50 +0530

    change the image of the cap

--- customization/src/App.js
+++ customization/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import CapCustomization from './components/CapCustomization';
+import Products from './components/Products';
 
 function App() {
   return (
     <div className="App">
-      <CapCustomization/>
+     <Products/>
     </div>
   );
 }
